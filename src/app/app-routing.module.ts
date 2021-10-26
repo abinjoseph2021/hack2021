21c8bd1370558b3b94d423ed06d4052dbf383e9e
abinjoseph2050 <92216512+abinjoseph2050@users.noreply.github.com>
2021-10-26 06:03:46 +0530

    Update app-routing.module.ts

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,43 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { Page404Component } from './Routes/page404/page404.component';
+import { AboutComponent } from './Routes/about/about.component';
+import { HomeComponent } from './Routes/home/home.component';
+import { UserComponent } from './Routes/user/user.component';
+import { AboutCompanyComponent } from './about-company/about-company.component';
+import { AboutMeComponent } from './about-me/about-me.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  {
+    component:HomeComponent,
+    path:''
+  },
+  {
+    component:AboutComponent,
+    path:'about',
+    children: [{
+      component:AboutCompanyComponent,
+      path:'company'
+    },
+    {
+      component:AboutMeComponent,
+      path:'me'
+    }]
+  },
+  {
+    component:UserComponent,
+    path:'user/:id'
+  },
+  {
+    component:Page404Component,
+    path:'**'
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
+
